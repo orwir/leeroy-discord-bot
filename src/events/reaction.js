0aea7b60942edb185716c6588a759c4b55a0f870
Igor Zubenko <Igor_Zubenko@epam.com>
2019-09-10 15:30:58 +0200

    #30: Fixed: bot added role to himself

--- src/events/reaction.js
+++ src/events/reaction.js
@@ -8,29 +8,27 @@ export const REACTION_REMOVED = 'MESSAGE_REACTION_REMOVE'
 export const REACTION_TYPES = [REACTION_ADD, REACTION_REMOVED]
 
 export default async function (bot, data, reacted) {
+    const user = bot.users.get(data.user_id)
     const channel = bot.channels.get(data.channel_id)
     const msg = await channel.fetchMessage(data.message_id)
-    if (msg.author.id !== bot.user.id) {
+    if (user.bot || msg.author.id !== bot.user.id) {
         return
     }
     if (msg.path(`${FEATURE_PATH}.name`) !== 'feature') {
-        return
-    }
-    const emoji = data.emoji.name
-    const author = msg.guild.member(bot.users.get(data.user_id))
-    if (author.bot) {
         return
     }
     const feature = features[msg.path(`${FEATURE_PATH}.value`)]
     if (!feature) {
         return
     }
+    const emoji = data.emoji.name
+    const member = msg.guild.member(user)
     if (reacted && !feature.emojis.includes(emoji)) {
         msg.reactions
             .find(reaction => reaction.emoji.name === emoji)
-            .remove(author)
+            .remove(member)
     } else {
-        feature.react(msg, emoji, author, reacted)
+        feature.react(msg, emoji, member, reacted)
             .catch(error => console.log(error))
     }
 }
