08370452402844b4daffe94e374332f95de011b0
orwir <igor.a.zubenko@gmail.com>
2020-11-26 21:25:57 +0100

    improted text utils

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,5 +1,3 @@
 const _emoji = /^(((?:<(:\w+:|a:\w+:)(\d+)>|(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]))(\s{0,1}?))+)$/
 
-export function isContentEmoji(text) {
-    return Boolean(_emoji.test(text) || _dsEmoji.test(text))
-}
+export function isContentEmoji(text) { return _emoji.test(text) }
