2692bd7fd17355c4567341e6c956b748dfb707a9
orwir <igor.a.zubenko@gmail.com>
2020-11-26 21:38:10 +0100

    and one more time regex update

--- src/utils/text.js
+++ src/utils/text.js
@@ -1,3 +1,3 @@
-const _emoji = /^(((?:<(:\w+:|a:\w+:)(\d+)>|(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]))(\s{0,1}?))+)$/
+const _emoji = /^(((?:<(:\w+:|a:\w+:)(\d+)>|(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]))(\s*?))+)$/
 
 export function isContentEmoji(text) { return _emoji.test(text) }
